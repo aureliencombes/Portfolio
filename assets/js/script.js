5d46ed963091f1047f333b116e6af70a338bc2f1
Aurélien Combes <aurelien.combes@hetic.net>
2018-02-26 03:07:19 +0100

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const lineMain = document.querySelector('.lineMain')
+const portfolio = document.querySelector('.portfolio')
+const works = document.querySelector('.works')
+const bio = document.querySelector('.bio')
+
+const annim = addEventListener('click', () => {
+
+    console.log('switch')
+
+    lineMain.classList.add('active')
+    portfolio.classList.add('active')
+    works.classList.add('active')
+    bio.classList.add('active')
+
+})
