2e90e9a8562131645fb63b4d5e570d029d388f69
Aurélien Combes <aurelien.combes@hetic.net>
2018-03-05 16:17:51 +0100

    +Tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,40 @@
-// const lineMain = document.querySelector('.lineMain')
-// const portfolio = document.querySelector('.portfolio')
-// const works = document.querySelector('.works')
-// const bio = document.querySelector('.bio')
+const menu = document.querySelector('.menu')
+const bioButton = document.querySelector('.bioButton')
+const worksButton = document.querySelector('.worksButton')
+const bar = document.querySelector('.bar')
+const cache1 = document.querySelector('.cache1')
+const cache2 = document.querySelector('.cache2')
+const bioText = document.querySelector('.bioText')
+const worksGallery = document.querySelector('.gallery')
 
-// const annim = addEventListener('click', () => {
 
-//     console.log('switch')
+const works = worksButton.addEventListener('click', () => {
 
-//     lineMain.classList.add('active')
-//     portfolio.classList.add('active')
-//     works.classList.add('active')
-//     bio.classList.add('active')
+    menu.classList.add('active')
+    bar.classList.add('active')
 
-// })
+    cache1.style.transform = "translateY(26%)"
+    cache2.style.display = "none"
+
+    bioText.classList.remove('active')
+    worksGallery.classList.add('active')
+
+    worksButton.style.color = 'black'
+    bioButton.style.color = 'rgb(228, 228, 228)'
+
+})
+
+const bio = bioButton.addEventListener('click', () => {
+
+    menu.classList.add('active')
+    bar.classList.add('active')
+
+    cache1.style.transform = "translateY(26%)"
+    cache2.style.display = "none"
+
+    worksGallery.classList.remove('active')
+    bioText.classList.add('active')
+
+    worksButton.style.color = 'rgb(228, 228, 228)'
+    bioButton.style.color = 'black'
+})
