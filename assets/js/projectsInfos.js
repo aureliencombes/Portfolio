faa49a1eb6a29d682f3c010974d69784a02e2643
Aurélien <aurelien.combes@hetic.net>
2018-04-28 02:38:03 +0200

    MQ + ProjectsPreview
    Medias queries optimisation + projects previews

---
+++ assets/js/projectsInfos.js
@@ -0,0 +1,73 @@
+const project = []
+
+    project[1] = {
+        image : 'url("assets/img/project/spaceXproject.jpg")',
+        title : 'Space X School Project',
+        text : 'This project has been made for a school test, where we had five days to make an experience about spaceX.',
+        linkText : 'test it',
+        link : 'https://aureliencombes.github.io/SpaceX/',
+        };
+
+    project[2] = {
+        image : 'url("assets/img/project/inrocks_index.jpg")',
+        title : 'inrocks School Project',
+        text : 'Made for a school team project where we had to make a redesign of the website "les Inrockuptible Festival 2017."',
+        linkText : 'show',
+        link : 'assets/img/project/inrocks_index.jpg',
+        },
+
+    project[3] = {
+        image : 'url("assets/img/project/alainalaric.jpg")',
+        title : 'professionnal school project',
+        text : 'Made for a school team project for a real client.',
+        linkText : 'discover',
+        link : 'www.alainalaric.ovh',
+        };
+
+    project[4] = {
+        image : 'url("assets/img/project/nikeHyperadapt.jpg")',
+        title : 'Nike Poster',
+        text : 'Pub poster for the Nike Hyper-Adapt.',
+        linkText : 'show',
+        link : 'assets/img/project/nikeHyperadapt.jpg',
+        };
+
+    project[5] = {
+        image : 'url("assets/img/project/nikitaPoster.jpg")',
+        title : "Film poster in Olly Moss's style",
+        text : "A redesign of Nikita film poster with a double meaning like Olly Moss's works.",
+        linkText : 'show',
+        link : 'assets/img/project/nikitaPoster.jpg',
+        };
+
+    project[6] = {
+        image : 'url("assets/img/project/Portrait.jpg")',
+        title : 'Auto portrait',
+        text : "Inspired by Roy Lichtenstein's works and mixed up with old VHS style. made with Illustrator / Photoshop.",
+        linkText : 'show',
+        link : 'assets/img/project/Portrait.jpg',
+        };
+
+    project[7] = {
+        image : 'url("assets/img/project/smk.jpg")',
+        title : 'Portait of a friend',
+        text : 'digital painting on Photoshop.',
+        linkText : 'show',
+        link : 'assets/img/project/smk.jpg',
+        };
+
+    project[8] = {
+        image : 'url("assets/img/project/guess_who.jpg")',
+        title : '3D Guess Who',
+        text : 'Made for a school project in CSS 3D.',
+        linkText : 'test it',
+        link : 'https://aureliencombes.github.io/Guess_Who_3D/',
+        };
+
+    project[9] = {
+        image : 'url("assets/img/project/canvas.jpg")',
+        title : 'Little canvas game',
+        text : "just a simple 2D game in canvas, you can't do many things. still in developpement",
+        linkText : 'test it',
+        link : 'https://aureliencombes.github.io/Canvas-Game/',
+        };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 const menu = document.querySelector('.menu')
+
 const bioButton = document.querySelector('.bioButton')
 const worksButton = document.querySelector('.worksButton')
+
+const closeButton = document.querySelector('.close')
+
 const bar = document.querySelector('.bar')
+
 const cache1 = document.querySelector('.cache1')
 const cache2 = document.querySelector('.cache2')
+
 const bioText = document.querySelector('.bioText')
 const worksGallery = document.querySelector('.gallery')
+
+const projectSelected = []
+const blackScreen = document.querySelector('.blackScreen')
+const projectImage = document.querySelector('.previewImage')
+const projectTitle = document.querySelector('.previewTitle')
+const projectText = document.querySelector('.previewText')
+const projectLink = document.querySelector('.previewLink')
 
 
 const works = worksButton.addEventListener('click', () => {
@@ -14,7 +27,7 @@ const works = worksButton.addEventListener('click', () => {
     bar.classList.add('active')
 
     cache1.classList.add('active')
-    cache2.style.display = "none"
+    cache2.style.display = 'none'
 
     bioText.classList.remove('active')
     worksGallery.classList.add('active')
@@ -24,17 +37,46 @@ const works = worksButton.addEventListener('click', () => {
 
 })
 
-const bio = bioButton.addEventListener('click', () => {
-
+const bio = bioButton.addEventListener('click', () => 
+{
     menu.classList.add('active')
     bar.classList.add('active')
 
     cache1.classList.add('active')
-    cache2.style.display = "none"
+    cache2.style.display = 'none'
 
     worksGallery.classList.remove('active')
     bioText.classList.add('active')
 
     worksButton.style.color = 'rgb(228, 228, 228)'
     bioButton.style.color = 'black'
+
 })
+
+const close = closeButton.addEventListener('click', () => 
+{
+    blackScreen.style.display = 'none'
+})
+
+const projectSelection = () => 
+{
+
+    for(let i = 1; i < 10; i++)
+    {
+        projectSelected[i] = document.querySelector(`.index-${i}`)
+        projectSelected[i].addEventListener('click', () => 
+        {
+            console.log(i)
+            blackScreen.style.display = 'inherit'
+
+            projectImage.style.backgroundImage = project[i].image
+            projectTitle.innerHTML = project[i].title
+            projectText.innerHTML = project[i].text
+            projectLink.innerHTML = project[i].linkText
+            projectLink.href = project[i].link
+        })
+    }
+}
+
+projectSelection()
+
